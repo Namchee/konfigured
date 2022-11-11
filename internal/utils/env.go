c1a0c25be374c8185c4f7c0d92fe462f2daabcbb
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-11-11 22:02:59 +0700

    feat: add newline option

--- internal/utils/env.go
+++ internal/utils/env.go
@@ -1,8 +1,24 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 // ReadEnvString returns string value from environment variables
 func ReadEnvString(key string) string {
 	return os.Getenv(key)
+}
+
+// ReadEnvBool read and parse boolean environment variables.
+// Will return `false` if the variable is not a `bool`
+func ReadEnvBool(key string) bool {
+	value := os.Getenv(key)
+	parsed, err := strconv.ParseBool(value)
+
+	if err != nil {
+		return false
+	}
+
+	return parsed
 }
